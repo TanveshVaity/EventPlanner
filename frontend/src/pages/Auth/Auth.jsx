ea1f9cf220ab93e5671207f450d0f2aab74a29a5
TanveshVaity <tanveshvaity99@gmail.com>
2023-09-15 02:13:39 +0530

    added login

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,40 +1,88 @@
-import React, { useCallback, useRef } from "react";
+import React, { useState, useRef } from "react";
+import Axios from "axios";
 import "./Auth.css";
 
 const Auth = () => {
-    const emailInput = useRef();
-    const passwordInput = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const emailInput = useRef();
+  const passwordInput = useRef();
 
-    const submitHandler = useCallback((event) => {
-        event.preventDefault();
-        const email = emailInput.current.value;
-        const password = passwordInput.current.value;
+  const switchModeHandler = () => {
+    setIsLogin((prevIsLogin) => !prevIsLogin);
+  };
 
-        if (email.trim().length === 0 || password.trim().length === 0) {
-            return;
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const email = emailInput.current.value;
+    const password = passwordInput.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
         }
-        console.log(email, password);
-        
-        emailInput.current.value = "";
-        passwordInput.current.value = "";
-    }, []);
+      `,
+    };
 
-    return (
-        <form className="auth-form" onSubmit={submitHandler}>
-            <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" ref={emailInput} />
-            </div>
-            <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" ref={passwordInput} />
-            </div>
-            <div className="form-actions">
-                <button type="submit">Submit</button>
-                <button type="button">Switch to Signup</button>
-            </div>
-        </form>
-    );
-}
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `,
+      };
+    }
+
+    try {
+      const response = await Axios.post(
+        "http://localhost:5000/api",
+        requestBody,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data)
+
+      emailInput.current.value = "";
+      passwordInput.current.value = "";
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" ref={emailInput} />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={passwordInput} />
+      </div>
+      <div className="form-actions">
+        <button type="submit">{isLogin ? "Login" : "Signup"}</button>
+        <button type="button" onClick={switchModeHandler}>
+          Switch to {isLogin ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default Auth;
