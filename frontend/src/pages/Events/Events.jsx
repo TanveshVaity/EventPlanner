1beef2d870a2b7d964db35513ebfce7e7f3b1315
TanveshVaity <tanveshvaity99@gmail.com>
2023-09-17 17:20:36 +0530

    added event model

--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -1,12 +1,47 @@
-import React from "react";
-import "./Events.css";
+import React, { useState } from 'react';
+import Backdrop from '../../components/Backdrop/Backdrop';
+import Modal from '../../components/Model/Modal';
+import './Events.css';
 
-const Events = () =>{
-    return(
-        <div className="event-container">
-            <h1>Events</h1>
+const Events = () => {
+  const [creating, setCreating] = useState(false);
+
+  const startCreateEventHandler = () => {
+    setCreating(true);
+  };
+
+  const modalConfirmHandler = () => {
+    setCreating(false);
+  };
+
+  const modalCancelHandler = () => {
+    setCreating(false);
+  };
+
+  return (
+    <React.Fragment>
+      {creating && <Backdrop />}
+      {creating && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onCancel={modalCancelHandler}
+          onConfirm={modalConfirmHandler}
+        >
+          <p>Modal Content</p>
+        </Modal>
+      )}
+      <div className="events-container">
+        <div className="events-control">
+            <p>Share your own Events!</p>
+            <button className="btn" onClick={startCreateEventHandler}>
+            Create Event
+            </button>
         </div>
-    )
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Events;
